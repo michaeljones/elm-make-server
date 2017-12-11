e1e8d21f7c200d88457f92f54e1f35e189c10525
Michael Jones <m.pricejones@gmail.com>
2017-12-11 20:05:12 +0000

    Initial build server of sorts
    We can run 'elm-make' in a different tab and our server will run
    elm-make for us and it seems to work providing it is set up in the right
    folder with the correct environment.

--- index.ts
+++ index.ts
@@ -1,20 +1,96 @@
+#!/usr/bin/env node
 
-import { spawn } from 'child_process'
+import { spawn, spawnSync } from 'child_process'
+import * as net from 'net'
 
-function main(args: string[]) {
+function daemon() {
+    const server = net.createServer(c => {
+        // 'connection' listener
+        console.log('client connected')
 
+        c.on('data', buffer => {
+            const str = buffer.toString('utf8')
+            console.log('receiving data', str)
+            const json = JSON.parse(str)
+
+            spawnSync('elm-make', [], { stdio: 'inherit' })
+
+            c.end()
+        })
+
+        c.on('end', () => {
+            console.log('client disconnected')
+        })
+    })
+    server.on('error', err => {
+        throw err
+    })
+    server.listen(3111, () => {
+        console.log('server bound')
+    })
+}
+
+async function isDaemonRunning() {
+    return new Promise((resolve, reject) => {
+        const client = net.createConnection({ port: 3111 }, () => {
+            console.log('Found daemon')
+            client.end()
+            resolve(true)
+        })
+
+        client.on('error', err => {
+            console.log('Failed to find daemon')
+            client.end()
+            resolve(false)
+        })
+    })
+}
+
+async function sendCompile(args: string[]) {
+    return new Promise((resolve, reject) => {
+        const client = net.createConnection({ port: 3111 }, () => {
+            console.log('Found daemon for sending args')
+        })
+
+        client.on('end', () => {
+            client.end()
+            resolve(true)
+        })
+
+        client.on('error', err => {
+            console.log('Failed to find daemon')
+            client.end()
+            resolve(false)
+        })
+
+        client.write(JSON.stringify(args), 'utf8')
+    })
+}
+
+async function main(args: string[]) {
     console.log(args)
 
     const nodeExe = args[0]
     const script = args[1]
     const isDaemon = args[2] === 'daemon'
 
-    if (!isDaemon) {
-        const options = {
-            stdio: 'inherit'
-        }
+    if (isDaemon) {
+        // If we're meant to be the daemon then set up the daemon
+        daemon()
+    } else {
+        const hasDaemon = await isDaemonRunning()
+        if (!hasDaemon) {
+            // If we can't find the daemon, then set up the daemon
+            console.log('Setting up daemon')
+            const options = {
+                stdio: 'inherit'
+            }
 
-        const childProcess = spawn('ts-node', [script, "daemon"], options)
+            const childProcess = spawn('node', [script, 'daemon'], options)
+        } else {
+            // We can find the daemon, sent it our args
+            sendCompile(args)
+        }
     }
 }
 
